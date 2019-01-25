110b488182c8908f32c59ece4ff92f28b4359976
Michael Huffman <huffman442@yahoo.com>
2019-01-25 15:24:55 -0500

    filled in the enrollments variable

--- ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolInitializer.cs
@@ -35,7 +35,24 @@ namespace ContosoUniversity.DAL
                 new Course{CourseID=2021, Title="Composition",Credits=3,},
                 new Course{CourseID=2042, Title="Literature",Credits=4,}
             };
-            courses.ForEach(s => ContextBoundObject.Courses.Add(s));
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+            var enrollments = new List<Enrollment>
+            {
+                new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+                new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+                new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+                new Enrollment{StudentID=1,CourseID=1045,Grade=Grade.B},
+                new Enrollment{StudentID=1,CourseID=3141,Grade=Grade.F},
+                new Enrollment{StudentID=1,CourseID=2021,Grade=Grade.F},
+                new Enrollment{StudentID=3,CourseID=1050},
+                new Enrollment{StudentID=4,CourseID=1050,},
+                new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+                new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+                new Enrollment{StudentID=6,CourseID=1045},
+                new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+            enrollments.ForEach(s => context.Enrollments.Add(s));
             context.SaveChanges();
         }
     }
